0bdeeb2d7f9d3369f2902d33c4c4691996a5d4eb
Pashkova Daria <dar.pashcovaa@gmail.com>
2023-09-12 15:36:01 +0900

    add hangman game files

---
+++ src/js/consts.js
@@ -0,0 +1,17 @@
+export const WORDS = [
+  "code",
+  "frontend",
+  "programming",
+  "network",
+  "host",
+  "detail",
+  "algorithm",
+  "argument",
+  "neuron",
+  "script",
+  "variable",
+  "terminal",
+  "statement",
+];
+
+export const KEYBOARD__LETTERS = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
